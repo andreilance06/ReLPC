d044eff7edb631024d974917ed76866ff6946f76
andreilance06 <andreilanceasuncion@gmail.com>
2026-05-06 22:30:34 +0800

    Handle invalid sign-up cases

--- ViewModels/SignUpWindowViewModel.cs
+++ ViewModels/SignUpWindowViewModel.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -25,6 +26,8 @@ public partial class SignUpWindowViewModel(
     [NotifyPropertyChangedFor(nameof(PasswordChar2))]
     public partial bool HidePassword2 { get; set; } = true;
 
+    [ObservableProperty] public partial string? SignupStatus { get; set; }
+
     public string PasswordChar1 => HidePassword1 ? "\u25cf" : string.Empty;
 
     public string PasswordChar2 => HidePassword2 ? "\u25cf" : string.Empty;
@@ -34,13 +37,28 @@ public partial class SignUpWindowViewModel(
     {
         if (string.IsNullOrEmpty(Username) || string.IsNullOrEmpty(Password) ||
             string.IsNullOrEmpty(PasswordConfirmation))
+        {
+            SignupStatus = "Fill in all user details!";
             return;
+        }
 
         if (!Username.All(c => char.IsDigit(c) || c == '-'))
+        {
+            SignupStatus = "Invalid ID! Example ID: 12-3456-789";
             return;
+        }
 
         if (Password != PasswordConfirmation)
+        {
+            SignupStatus = "Passwords don't match!";
             return;
+        }
+
+        if (databaseService.GetProfileByUsername(Username) is not null)
+        {
+            SignupStatus = "ID already registered!";
+            return;
+        }
 
         var profile = new UserProfile
         {
@@ -48,8 +66,15 @@ public partial class SignUpWindowViewModel(
             PasswordHash = BCrypt.Net.BCrypt.EnhancedHashPassword(Password),
         };
 
-        databaseService.UpsertUser(profile);
-
-        windowService.FindWindowFromDataModel(this)?.Close();
+        try
+        {
+            databaseService.UpsertUser(profile);
+            SignupStatus = "Successfully signed up!";
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            SignupStatus = "An error occured while signing up!";
+        }
     }
 }
