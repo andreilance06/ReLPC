4a0a47287d892689fecc5875c7b608871e33fb02
andreilance06 <andreilanceasuncion@gmail.com>
2026-05-06 21:02:05 +0800

    Implement toggling visibility of passwords

--- ViewModels/SignUpWindowViewModel.cs
+++ ViewModels/SignUpWindowViewModel.cs
@@ -17,6 +17,18 @@ public partial class SignUpWindowViewModel(
 
     [ObservableProperty] public partial string? PasswordConfirmation { get; set; }
 
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(PasswordChar1))]
+    public partial bool HidePassword1 { get; set; } = true;
+
+    [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(PasswordChar2))]
+    public partial bool HidePassword2 { get; set; } = true;
+
+    public string PasswordChar1 => HidePassword1 ? "\u25cf" : string.Empty;
+
+    public string PasswordChar2 => HidePassword2 ? "\u25cf" : string.Empty;
+
     [RelayCommand]
     private void AttemptSignup()
     {
